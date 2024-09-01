444e1701e7a7bf11b0e09c99767f06c7d9360b1a
Alexandru Ungur <alexaandru@gmail.com>
2024-09-01 10:09:34 +0300

    Updated parsers

---
+++ tsx/highlights.scm
@@ -0,0 +1,39 @@
+; Types
+
+(type_identifier) @type
+(predefined_type) @type.builtin
+
+((identifier) @type
+ (#match? @type "^[A-Z]"))
+
+(type_arguments
+  "<" @punctuation.bracket
+  ">" @punctuation.bracket)
+
+; Variables
+
+(required_parameter (identifier) @variable.parameter)
+(optional_parameter (identifier) @variable.parameter)
+
+; Keywords
+
+[ "abstract"
+  "declare"
+  "enum"
+  "export"
+  "implements"
+  "interface"
+  "keyof"
+  "namespace"
+  "private"
+  "protected"
+  "public"
+  "type"
+  "readonly"
+  "override"
+  "satisfies"
+] @keyword
+
+; Sub-language delimeters
+(glimmer_opening_tag) @tag.builtin
+(glimmer_closing_tag) @tag.builtin
