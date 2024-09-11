bbb8f5c271cf71688f12fc988445f75618b0d2ac
Alexandru Ungur <alexaandru@gmail.com>
2024-09-11 15:19:56 +0300

    Updated parsers

--- flamingo/highlights.scm
+++ flamingo/highlights.scm
@@ -1,49 +1,48 @@
 ; This Source Form is subject to the terms of the AQUA Software License,
 ; v. 1.0. Copyright (c) 2024 Aymeric Wibo
 
+; The order of these captures is important; the last match is the one that is used.
+; So start off with all the matches on primitive constructs, and then do the more complex stuff after.
+
 ; Comments.
 
 (comment) @comment
 (doc_comment) @comment.documentation
 
-; function calls
-
-(call
-	callable: (expression (identifier)) @function
-)
-
-; builtins
-
-[
-	"print"
-	"assert"
-] @function.builtin
-
-; identifiers
+; Identifiers.
 
 (identifier) @variable
 
-; operators
+; Literals.
+
+(string) @string
+(number) @number
+(bool) @constant.builtin
+(none) @constant.builtin
+
+; Operators.
 
 (overloadable_operator) @operator
 
-; declarations
+[
+	"**"
+	"*"
+	"/"
+	"%"
+	"+"
+	"-"
+	"=="
+	"!="
+	"<"
+	">"
+	"<="
+	">="
+	"&&"
+	"||"
+	"^^"
+] @operator
 
-(function_declaration name:
-	[
-		(identifier)
-		(overloadable_operator)
-	] @function
-)
-(class_declaration name: (identifier) @function)
-
-; operators
-
-; [
-; 	"+"
-; ] @operator
-
-; keywords
+; Keywords.
 
 [
 	"extern"
@@ -52,9 +51,17 @@
 	"class"
 	"import"
 	"return"
+	"let"
 ] @keyword
 
-; types
+; Builtins.
+
+[
+	"print"
+	"assert"
+] @function.builtin
+
+; Types.
 
 (type_name) @type
 [
@@ -63,9 +70,23 @@
 	"map"
 ] @type.builtin
 
-; literals
+; Function calls.
 
-(string) @string
-(number) @number
-(bool) @constant.builtin
-(none) @constant.builtin
+(call
+	callable: (expression (identifier) @function.method)
+)
+
+(call
+	callable: (expression (access accessor: (identifier) @function.method))
+)
+
+; Declarations.
+
+(var_decl name: (identifier) type: (type) @variable)
+(function_declaration name:
+	[
+		(identifier)
+		(overloadable_operator)
+	] @function
+)
+(class_declaration name: (identifier) @function)
