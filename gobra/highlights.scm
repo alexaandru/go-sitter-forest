9beecbf4c50beac989e92de5196105be4eaf4df3
Alexandru Ungur <alexaandru@gmail.com>
2024-11-29 09:46:45 +0200

    Updated parsers

--- gobra/highlights.scm
+++ gobra/highlights.scm
@@ -4,6 +4,13 @@
 (field_identifier) @property
 (identifier) @variable
 
+(const_declaration
+  (const_spec
+    name: (identifier) @constant))
+
+(adt_clause
+  variant: (_) @constructor)
+
 ; Function calls
 
 (call_expression
@@ -11,7 +18,12 @@
 
 (call_expression
   function: (identifier) @function.builtin
-  (#match? @function.builtin "^(append|cap|close|complex|copy|delete|imag|len|make|new|panic|print|println|real|recover|acc|old)$"))
+  (#match? @function.builtin "^(append|cap|close|complex|copy|delete|imag|len|make|new|panic|print|println|real|recover|acc|old|before)$"))
+
+(call_expression
+  function: (identifier) @constructor
+  (#match? @constructor "^(new.|New)"))
+
 
 (call_expression
   function: (selector_expression
@@ -103,6 +115,7 @@
   "return"
   "select"
   "struct"
+  "adt"
   "switch"
   "type"
   "var"
